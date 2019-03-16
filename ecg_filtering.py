09a580eb2f5be8d6999744423707f8aa94a8188f
Rushike Supreme <rushike.ab1@gmail.com>
2019-03-16 17:31:01 +0530

    Just modified

--- ecg_filtering.py
+++ ecg_filtering.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas
 import scipy.signal as signal
 from biosppy.signals import ecg
+from sklearn.preprocessing import StandardScaler
 # from scipy.fftpack import fft, fft2, fftshift
 import numpy
 
@@ -26,17 +27,32 @@ def fft_(xn):
 def fft_freq(xn):
     return numpy.fft.fftfreq(xn.shape[-1])
 
+def diff(xn):
+    le = len(xn) - 1
+    yn = numpy.array([])
+    for i in range(le):
+        di = xn[i + 1] - xn[i]
+        yn = numpy.append(yn, di)
+    yn = numpy.append(yn, 0)
+    print('diff : ', len(yn))
+    return yn
+
+def standardise(xn):
+    scaler = StandardScaler().fit(xn)
+    return scaler.transform(xn)
+
 def get_qrs(xn, sampling_rate = 250):
     out = ecg.ecg(xn, sampling_rate=250., show=False)
     rpeaks, xnr = r_peaks(xn, 250)
     le = len(xn)
+    thr = numpy.max(xn) * 0.70
     #get q's
     xnq, q = [], []
     for rin in rpeaks:
         ind = rin
         active = False
         while True:
-            if xn[ind] < 0: active = True
+            if xn[ind] < thr: active = True
             if active and xn[ind - 1] > xn[ind] : 
                 xnq.append(xn[ind])
                 q.append(ind)
@@ -48,19 +64,32 @@ def get_qrs(xn, sampling_rate = 250):
         ind = rin
         active = False
         while True:
-            if xn[ind] < 0: active = True
+            if xn[ind] < thr: active = True
             if active and xn[ind - 1] < xn[ind] : 
                 xns.append(xn[ind - 1])
                 s.append(ind - 1)
                 break
             ind += 1
- 
- 
-    return {'q_ts' : q, 'q' : xnq, 'r_ts' : rpeaks, 'r' : xnr, 's_ts' : s, 's' : xns}
+    #get qrs duration
+    du_ts = []
+    for jt in range(len(q)):    
+        # in1 = q[jt]
+        # while True:
+        #     if xn[in1] > thr:
+        #         break
+        #     in1 -= 1
+        # in2 = s[jt]
+        # while True:
+        #     if xn[in2] > thr:
+        #         break
+        #     in2 += 1
+        # du_ts.append((in2 - in1) / 250)
+        du_ts.append((s[jt] - q[jt]) / sampling_rate)
+    return {'q_ts' : q, 'q' : xnq, 'r_ts' : rpeaks, 'r' : xnr, 's_ts' : s, 's' : xns, 'du_ts' : du_ts}
 
 
 def get_heart_rate(xn, sampling_rate = 250, show = False):
-    out = ecg.ecg(xn, sampling_rate=250., show=show)
+    out = ecg.ecg(xn, sampling_rate=250., show=False)
     # print(out)
     return out['heart_rate_ts'] , out['heart_rate']
 def r_peaks(xn, sampling_rate = 250, show = False):
@@ -69,32 +98,51 @@ def r_peaks(xn, sampling_rate = 250, show = False):
     val = [xn[i] for i in r_p]
     return r_p, val
 if __name__ == '__main__':
-    dataset = pandas.read_csv('./csv/a103l.csv').values
-    print(dataset[:1250,1])
-    xn = dataset[:1250, 1]
-    it = dataset[:1250, 0]
+    dataset = pandas.read_csv('./csv/a386s.csv').values
+    # print(dataset[:1000,0:1])
+    xn = dataset[:10000,1]
+    it = dataset[:10000, 0]
+    plt.plot(it, xn, label = 'signal')
+    yn = diff(xn)
+    plt.plot(it, yn, label = 'diff')
+    plt.legend()
+    plt.show()
+
+    # xn = low_pass_filter(xn, 20, 250)
+    # xn = standardise(numpy.array(dataset[:1000, 0:1])).flat
+
+   
+    
+    # print(xn)
     # out = ecg.ecg(xn, sampling_rate=250., show=True)
     # print(out['heart_rate'])
-    out = get_heart_rate(xn, 250)
-    ct = get_qrs(xn,250)
-    print(out[1][5])
-    # print(get_heart_rate(xn, 250))
-    plt.plot(it, xn, label = 'signal')
+    # out = get_heart_rate(xn, 250, False)
+    # ct = get_qrs(xn,250)
+    # # print(out[1][5])
+    # print(ct)
+    # # print(get_heart_rate(xn, 250))
+    # plt.plot(it, xn, label = 'ECG Signal')
+    # plt.title("QRS  Plot")
+    # plt.xlabel("Time in ms")
     
-    plt.plot(ct['q_ts'], ct['q'], label = 'signal')
+    # plt.plot(ct['q_ts'], ct['q'], 'bs', label = "Q peaks",  )
     
-    plt.plot(ct['r_ts'], ct['r'], label = 'signal')
+    # plt.plot(ct['r_ts'], ct['r'], 'r--' , label = "R peaks", color = "black", linewidth = 3.0 )
     
-    plt.plot(ct['s_ts'], ct['s'], label = 'signal')
-    # yn = low_pass_filter(xn, 12, 250, 6)
-    # plt.plot(it, yn, label= 'filtered signal')
-    # yn1 = high_pass_filter(yn, 12, 250, 6)
-    # plt.plot(it, yn1, label='High pass')
-    plt.show()
-    # # fft = fft_(xn)
-    # print(fft)
-    # freq = fft_freq(xn)
-    # print(freq)
-    # plt.plot(freq, fft.real,freq, fft.imag)
+    # plt.plot(ct['s_ts'], ct['s'], 'g^', label= "S peaks")
+    # plt.ylabel("ECG readings in mV")
+    # # plt.legend("Yellow - R peaks\nOrange - Q peaks\nGreen - S")
+    # plt.legend()
+    # print(get_heart_rate(xn, 250, True))
+    # # yn = low_pass_filter(xn, 12, 250, 6)
+    # # plt.plot(it, yn, label= 'filtered signal')
+    # # yn1 = high_pass_filter(yn, 12, 250, 6)
+    # # plt.plot(it, yn1, label='High pass')
     # plt.show()
+    # # # fft = fft_(xn)
+    # # print(fft)
+    # # freq = fft_freq(xn)
+    # # print(freq)
+    # # plt.plot(freq, fft.real,freq, fft.imag)
+    # # plt.show()
     
--- falsefiles.py
+++ falsefiles.py
@@ -1,20 +1,23 @@
 import pandas as pd
 import glob
-path=r'E:\TE\VI\false_alarm_reduction\training'
-allFiles=glob.glob(path + "\*.hea")
-allf=0
-falf=0
-word='#False'
-falselist=[]
-for file in allFiles:
-    allf+=1
-    f=open(file,'r')
-    line=f.readlines()
-    for x in line:
-            if word in x:
-                falf+=1
-                fname=file
-                falselist.append(fname[-9:-4])
-print('All header files : ',allf)
-print('All false alarm files : ',falf)
-print(falselist)
+path=r'./training'
+
+def all_false_files(path, all = False):
+    allFiles=glob.glob(path + "\*.hea")
+    allf=0
+    falf=0
+    word='#False'
+    falselist=[]
+    for file in allFiles:
+        allf+=1
+        f=open(file,'r')
+        line=f.readlines()
+        for x in line:
+                if word in x:
+                    falf+=1
+                    fname=file
+                    falselist.append(fname[-9:-4])
+    if all : return falselist, allf, falf
+    return falselist
+
+print(all_false_files(path))
